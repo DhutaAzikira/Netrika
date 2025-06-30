24587b4d37adbe36ce1c17502bdb957da386322f
Duta <dhuta.azikiras@gmail.com>
2025-06-30 21:39:24 +0700

    feat: Trigger first deployment

--- platform_app/views.py
+++ platform_app/views.py
@@ -232,7 +232,7 @@ def user_profile_api(request):
         404: OpenApiResponse(description="User profile not found."),
     }
 )
-@api_view(['PUT', 'PATCH']) #test1
+@api_view(['PUT', 'PATCH']) #test2
 @permission_classes([IsAuthenticated])
 def update_profile_api(request):
     user = request.user
